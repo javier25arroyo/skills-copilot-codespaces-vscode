1c3ab9a1424902973a5f51ef592d9b78b2952aa1
Javier Arroyo Rojas <86391507+javier25arroyo@users.noreply.github.com>
2024-06-15 04:46:57 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,61 @@
+//Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//Create a function that will return a random number between 1 and 10
+const getRandomNumber = () => {
+    return Math.ceil(Math.random() * 10);
+};
+
+//Create a function that will return a random number between 1 and 100
+const getRandomNumber100 = () => {
+    return Math.ceil(Math.random() * 100);
+};
+
+//Create a function that will return a random number between 1 and 1000
+const getRandomNumber1000 = () => {
+    return Math.ceil(Math.random() * 1000);
+};
+
+//Create a function that will return a random number between 1 and 10000
+const getRandomNumber10000 = () => {
+    return Math.ceil(Math.random() * 10000);
+};
+
+//Create a function that will return a random number between 1 and 100000
+const getRandomNumber100000 = () => {
+    return Math.ceil(Math.random() * 100000);
+};
+
+//Create a function that will return a random number between 1 and 1000000
+const getRandomNumber1000000 = () => {
+    return Math.ceil(Math.random() * 1000000);
+};
+
+//Create a function that will return a random number between 1 and 10000000
+const getRandomNumber10000000 = () => {
+    return Math.ceil(Math.random() * 10000000);
+};
+
+//Create a function that will return a random number between 1 and 100000000
+const getRandomNumber100000000 = () => {
+    return Math.ceil(Math.random() * 100000000);
+};
+
+//Create a function that will return a random number between 1 and 1000000000
+const getRandomNumber1000000000 = () => {
+    return Math.ceil(Math.random() * 1000000000);
+};
+
+//Create a function that will return a random number between 1 and 10000000000
+const getRandomNumber10000000000 = () => {
+    return Math.ceil(Math.random() * 10000000000);
+};
+
+//Create a function that will return a random number between 1 and 100000000000
+const getRandomNumber100000000000 = () => {
+    return Math.ceil(Math.random() * 100000000000);
+};
+
+//Create a function that will return a random number between 1 and 1000000000000
